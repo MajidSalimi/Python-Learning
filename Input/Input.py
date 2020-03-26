57a2e0ff4caa0e394beb1f4f5f294737140d0f3b
MajidSalimi <majidsalimib@gmail.com>
2020-03-26 12:05:09 +0430

    Initial Commit: different inputs from user

---
+++ Input/Input.py
@@ -0,0 +1,8 @@
+name=input('Please Enter Your Name:')
+number=int(input('Please enter your age:'))
+height=float(input('Please enter your age:'))
+
+print('Name:',name.title())
+print('Age:',number)
+print('Height:',height)
+print('Age/10=',eval('number/10'))
